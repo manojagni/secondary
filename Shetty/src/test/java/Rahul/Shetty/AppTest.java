0e059b3e91e440003be7c99354381b5163aa0bbb
manojagni <er.manojagnihotri@gmail.com>
2022-09-24 11:50:50 -0400

    first ever

---
+++ Shetty/src/test/java/Rahul/Shetty/AppTest.java
@@ -0,0 +1,14 @@
+package Rahul.Shetty;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+     @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("This is app test .java file ");   }
+}
---
+++ Shetty/src/test/java/Rahul/Shetty/test2.java
@@ -0,0 +1,20 @@
+package Rahul.Shetty;
+
+import org.junit.Test;
+
+public class test2 {
+	@Test
+	void Appium()
+	{
+		
+		System.out.println("This is appium test");
+	}
+	
+	@Test
+	void Mobile()
+	{
+		
+		System.out.println("This is Mobile test");
+	}
+
+}
